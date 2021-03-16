de16e6e9e32b57fd5b6d56c3764219856c52b867
Popovsky Alexey <pad19051999@gmail.com>
2021-03-16 18:53:47 +0200

    Add all changing

--- src/components/SignUpForm/SignUpFormItem/index.js
+++ src/components/SignUpForm/SignUpFormItem/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './SignUpFormItem.module.scss';
 import {ErrorMessage, Field} from 'formik';
 import InputMask from 'react-input-mask';
 import camelCase from 'camelcase';
+import {passwordComplexity} from '../../../config/signUp';
 
 const SignUpFormItem = ({fieldData, countryPhoneData, handleChange, values}) => {
+  const passwordComplexityNameArray = Object.keys(passwordComplexity);
   const {title, type, tag} = fieldData;
   const fieldId = camelCase(title);
+  const [passwordValue, setPasswordValue] = useState('');
+  const [passwordType, setPasswordType] = useState('');
+  const passwordHandleChange = event => {
+    handleChange(event);
+    setPasswordValue(event.target.value);
+    switch (event.target.value) {
+      case event.target.value.match(passwordComplexity.light)?.[0]: {
+        setPasswordType(passwordComplexityNameArray[0]);
+        break;
+      }
+      case event.target.value.match(passwordComplexity.medium)?.[0]: {
+        setPasswordType(passwordComplexityNameArray[1]);
+        break;
+      }
+      case event.target.value.match(passwordComplexity.strong)?.[0]: {
+        setPasswordType(passwordComplexityNameArray[2]);
+        break;
+      }
+      default:
+        setPasswordType('');
+        break;
+    }
+    console.log(event.target.value);
+    console.log(passwordType);
+  };
   return (
     <>
-      <div className={`${styles.box} ${styles[type]}`}>
-        <label className={styles.title} htmlFor={fieldId}>
+      <div className={`${styles.box} ${styles[type] || ''}`}>
+        <label className={`${styles.title} ${styles[passwordType] || ''}`} htmlFor={fieldId}>
           {fieldId === 'acceptPrivacyPolicy' ?
             <>
               {`${title.split(' ').shift()} `}
@@ -18,18 +45,26 @@ const SignUpFormItem = ({fieldData, countryPhoneData, handleChange, values}) => 
             </>
             : title}
         </label>
-        <Field className={styles.field}
-               id={fieldId}
-               as={tag}
-               type={type}
-               name={fieldId}
-               children={tag === 'select' ?
-                 <>
-                   <option value="" disabled>Select your country</option>
-                   {countryPhoneData.map(item => {
-                     return <option key={item.code}>{`${item.name} (${item.mask.match(/\+\d*/).join('')})`}</option>;
-                   })}
-                 </> : null}/>
+        {fieldId === 'password' ?
+          <Field className={styles.field}
+                 id={fieldId}
+                 type={type}
+                 name={fieldId}
+                 value={passwordValue}
+                 onChange={passwordHandleChange}/> :
+          <Field className={styles.field}
+                 id={fieldId}
+                 as={tag}
+                 type={type}
+                 name={fieldId}
+                 children={tag === 'select' ?
+                   <>
+                     <option value="" disabled>Select your country</option>
+                     {countryPhoneData.map(item => {
+                       return <option className={styles.option} key={item.code}>{`${item.name} (${item.mask.match(/\+\d*/).join('')})`}</option>;
+                     })}
+                   </> : null}/>}
+
         {type === 'checkbox' && <div/>}
         <ErrorMessage className={styles.error} name={fieldId} component="div"/>
       </div>
--- src/config/signUp.js
+++ src/config/signUp.js
@@ -63,17 +63,23 @@ export const signUpItemsArray = [
   },
 ];
 
+export const passwordComplexity = {
+  light: /^((?=.*[a-z])([a-z])){10,}|((?=.*[A-Z])([A-Z])){10,}|((?=.*[\d])([\d])){10,}|((?=.*[!-/:-@[-`{-~]))([!-/:-@[-`{-~]){10,}$/g,
+  medium: /^(?=.*[a-z])(?=.*[A-Z])([a-zA-Z]){10,}|(?=.*[a-z])(?=.*[\d])([a-z\d]){10,}|(?=.*[a-z])(?=.*[!-/:-@[-`{-~])([a-z!-/:-@[-`{-~]){10,}|(?=.*[\d])(?=.*[A-Z])([\dA-Z]){10,}|(?=.*[\d])(?=.*[!-/:-@[-`{-~])([\d!-/:-@[-`{-~]){10,}|(?=.*[A-Z])(?=.*[!-/:-@[-`{-~])([A-Z!-/:-@[-`{-~]){10,}$/g,
+  strong: /^(?=.*[!-/:-@[-`{-~\d])(?=.*[\w])([\w!-/:-@[-`{-~]){10,}$/g,
+}
+
 export const signUpValidationSchema = (phoneMaskLength) => yup.object().shape({
   firstName: yup.string().required('First name is required'),
   lastName: yup.string().required('Last name is required'),
   email: yup.string().email().required('Email is required'),
   sendMeEmailInPlaneText: yup.boolean(),
   personalPhoneNumber: yup.string().required('Personal phone number is required'),
-  personalPhoneNumberField: yup.string().required(),
+  personalPhoneNumberField: yup.string().required('This field is required'),
   companyName: yup.string().required('Company name is required'),
   companyAddress: yup.string().required('Company address is required'),
   companyPhoneNumber: yup.string().required('Company phone number is required'),
-  companyPhoneNumberField: yup.string().required(),
+  companyPhoneNumberField: yup.string().required('This field is required'),
   jobTitle: yup.string().required('Job title is required'),
   password: yup.string().min(10).required('Password is required'),
   confirmPassword: yup.string().oneOf([yup.ref('password'), null]).required('Confirm password is required'),
